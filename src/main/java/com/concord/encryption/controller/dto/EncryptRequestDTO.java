f2d960eb5c9b35bf1b4df284a74aedb45f0ecb10
VictoriaMirskaya <vik.mirskaya@gmail.com>
2021-03-22 11:38:51 +0200

    Refactoring.

--- src/main/java/com/concord/encryption/controller/dto/EncryptRequestDTO.java
+++ src/main/java/com/concord/encryption/controller/dto/EncryptRequestDTO.java
@@ -2,7 +2,6 @@ package com.concord.encryption.controller.dto;
 
 import lombok.Data;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 public class EncryptRequestDTO {
